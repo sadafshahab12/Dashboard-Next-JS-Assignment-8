55827b665ce4302843c9eead125bf197e14a4654
sadafshahabsr12 <sadafshahabsr12@gmail.com>
2024-11-12 07:44:26 +0500

    done

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -26,7 +26,6 @@ export default function EditInvoiceForm({
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
